4850419335038b9f36b1223c644d60938ab9918f
elzzup <guild0105@gmail.com>
2015-02-20 16:17:25 +0900

    check

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,11 +8,12 @@ require_once __DIR__.'/../models/Artist.php';
 
 if (file_exists('../.env.php')) {
     require_once('../.env.php');
+
+    // TODO: delete
+    ini_set('display_errors', '1');
+    error_reporting(E_ALL);
 }
 
-// TODO: delete
-ini_set('display_errors', '1');
-error_reporting(E_ALL);
 
 // Slimの初期化
 $app = new \Slim\Slim();
@@ -20,17 +21,16 @@ $app = new \Slim\Slim();
 // Slimにルート登録
 \Route::registration($app);
 
-// 実行
-$app->run();
-
 $connections = array(
   'master' => getenv('DATABASE_URL'),
 );
+var_dump($connections);
+$cfg->set_connections(array('development' => 'mysql://username:password@localhost/database_name'));
 
 ActiveRecord\Config::initialize(function($cfg) use ($connections) {
   $cfg->set_connections($connections);
-
-  # default connection is now production
   $cfg->set_default_connection('master');
 });
+
+$app->run();
 
